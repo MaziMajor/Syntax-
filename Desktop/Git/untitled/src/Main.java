47289eb015054f8d5ef8638410866eab65555196
Maziar Mahjor <maziarm25@gmail.com>
2023-01-09 20:59:53 -0500

    V6

--- Desktop/Git/untitled/src/Main.java
+++ Desktop/Git/untitled/src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("im the best ever");
         System.out.println("im gonna be fine");
         System.out.println("im gonna be so happy");
+        System.out.println("everything going well dont worry");
     }
 }
